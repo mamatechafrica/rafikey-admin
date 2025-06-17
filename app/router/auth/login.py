9ced7e3fd88c056fd7afc6e146df0631c0a6c109
kush2022 <felixkuria12@gmail.com>
2025-06-17 15:12:06 +0300

    requirements.txt updated

--- app/router/auth/login.py
+++ app/router/auth/login.py
@@ -14,7 +14,7 @@ from ...core.database import SessionDep
 
 load_dotenv()
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
